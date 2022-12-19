b8861645ee5544420e13ea65d916758e53a1a42c
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-19 22:40:44 +0330

    ShortTermAccount class

---
+++ src/main/java/ir/maktab/data/ShortTermAccount.java
@@ -0,0 +1,22 @@
+package ir.maktab.data;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Entity
+public class ShortTermAccount extends Account {
+    private double profit;
+
+    public ShortTermAccount(String cardNumber, String accountNumber, Date openingDate, Date expiryDate, double amount,
+                            int cvv2, double profit) {
+        super(cardNumber, accountNumber, openingDate, expiryDate, amount, cvv2);
+        this.profit = profit;
+    }
+}
