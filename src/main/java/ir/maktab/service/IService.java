aa66e4e9f667ad56f0e1b49ccf1f8bc960312dd4
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-20 00:03:47 +0330

    IService interface

--- src/main/java/ir/maktab/service/IService.java
+++ src/main/java/ir/maktab/service/IService.java
@@ -1,7 +1,12 @@
 package ir.maktab.service;
 
 import ir.maktab.data.Account;
+import ir.maktab.util.exception.AccountException;
 
 public interface IService<T extends Account> {
-    void createNewAccount(T account);
+    void createNewAccount(T account) throws AccountException;
+
+    void withdraw(T account, double amount);
+
+    void deposit(T account, double amount);
 }
