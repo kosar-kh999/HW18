f6e01fc0f85116dd7cf4a2af55f689896c8bfe9c
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-19 23:14:19 +0330

    EntityManagerFactory class

---
+++ src/main/java/ir/maktab/repository/EntityManagerFactory.java
@@ -0,0 +1,21 @@
+package ir.maktab.repository;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class EntityManagerFactory {
+    private static final EntityManagerFactory instance = new EntityManagerFactory();
+
+    private EntityManagerFactory() {
+    }
+
+    public static EntityManagerFactory getInstance() {
+        return instance;
+    }
+
+    public SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration().configure();
+        return configuration.buildSessionFactory();
+
+    }
+}
