c0daefd533bc48d0d4a49414bfb4859d1a105ed2
kosar khodadadi <kosar.khodadadi999@gmail.com>
2022-12-20 00:03:12 +0330

    AccountValidation class

--- src/main/java/ir/maktab/util/validation/AccountValidation.java
+++ src/main/java/ir/maktab/util/validation/AccountValidation.java
@@ -1,11 +1,13 @@
 package ir.maktab.util.validation;
 
+import ir.maktab.util.exception.AccountException;
+
 import javax.xml.bind.ValidationException;
 
 public class AccountValidation {
-    public static void validCardNumber(String cardNumber) throws ValidationException {
+    public static void validCardNumber(String cardNumber) throws AccountException {
         if (!cardNumber.equals("") && cardNumber.matches("^[0-9]{16}"))
             return;
-        throw new ValidationException("Card number should be 16 digits");
+        throw new AccountException("Card number should be 16 digits");
     }
 }
